92235c4748edfff19efe667e3ce2cb7c246a29cb
Lucas Josué Alves <52300483+LucasJa1994@users.noreply.github.com>
2025-04-08 12:02:21 -0300

    AuthGuard feito

---
+++ backend/middlewares/authGuard.js
@@ -0,0 +1,24 @@
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.JWT_SECRET
+
+const authGuard = async (req, res, next) => {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split("")[1]
+
+  // checar se no cabeçalho token existe
+  if (!token) return res.status(401).json({ errors: ["acesso negado"] })
+
+  // checar se o token é válido
+
+  try {
+    const verified = jwt.verify(token, jwtSecret)
+    // adiciona o usuário verificado ao objeto de requisição
+    req.user = await User.findById(verified.id).select("-password")
+    next()
+  } catch (error) {
+    return res.status(401).json({ errors: ["token inválido"] })
+  }
+}
+
+module.exports = authGuard
