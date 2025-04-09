cb5b40deb90b8d2e5a07cd01fd2c6d1a26fa8b4a
Lucas Josué Alves <52300483+LucasJa1994@users.noreply.github.com>
2025-04-09 19:43:10 -0300

    update de usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+const { default: mongoose } = require("mongoose")
 const jwtSecret = process.env.JWT_SECRET
 
 // GENERATE TOKEN user
@@ -86,7 +87,40 @@ const getCurrentUser = async (req, res) => {
 // update user
 
 const update = async (req, res) => {
-  res.send("update")
+  try {
+    const { name, password, bio } = req.body // pega os dados do body
+    let profileImage = null // cria uma variavel para receber a imagem de perfil
+    // verifica se o usuario existe
+    if (req.file) {
+      profileImage = req.file.filename // pega o nome do arquivo
+    }
+    const reqUser = req.user
+
+    const user = await User.findById(
+      new mongoose.Types.ObjectId(reqUser._id)
+    ).select("-password") //  pega o usuario que está logado e remove a senha do objeto
+
+    if (name) {
+      user.name = name // se o nome for diferente do que está no banco, atualiza o nome
+    }
+    if (password) {
+      const salt = await bcrypt.genSalt()
+      const passwordHash = await bcrypt.hash(password, salt) // gera o hash da senha
+      user.password = passwordHash // atualiza a senha
+    }
+
+    if (profileImage) {
+      user.profileImage = profileImage // atualiza a imagem de perfil
+    }
+    if (bio) {
+      user.bio = bio // atualiza a bio
+    }
+    await user.save() // salva as alterações no banco de dados
+    res.status(200).json(user) // retorna o usuario atualizado
+  } catch (error) {
+    console.error("Erro ao atualizar perfil:", error)
+    res.status(500).json({ message: "Erro interno ao atualizar perfil" })
+  }
 }
 module.exports = {
   register,
